f22ffe2045ed4a70ad7f7a6fdd8c816994b976ad
goncaloBarros25061998 <goncalo.santiago.barros@outlook.com>
2022-09-14 19:03:37 +0100

    New changes made; Test were created ; need to sort out the rest of the test to finish task 1.

--- src/main/java/com/sparta/goncalo/Main.java
+++ src/main/java/com/sparta/goncalo/Main.java
@@ -4,17 +4,17 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-            int [] array ={4, 1, 3, 9, 7};
-            int len = array.length;
-           // sortArray(array, len);
+        int[] array = {4, 1, 3, 9, 7};
+        // int len = array.length;
+        // sortArray(array, len);
 
-         Sort sorte = new Sort();
-          array = sorte.sortArray(array, len);
+        Sort sorte = new Sort();
+        array = sorte.bubbleSort(array);
 
 
-            for(int i = 0; i<len; ++i){
-                System.out.println(array[i]+ " ");
-            }
+        for (int i = 0; i < array.length; ++i) {
+            System.out.println(array[i] + " ");
+        }
 
         System.out.println(sorte);
 
--- src/main/java/com/sparta/goncalo/Sort.java
+++ src/main/java/com/sparta/goncalo/Sort.java
@@ -2,17 +2,31 @@ package com.sparta.goncalo;
 
 public class Sort implements Sorter {
     @Override
-    public int[] sortArray(int[] arrayToSort, int len) {
-
-        for (int i=0; i<len-1; ++i){
-            for (int j=0; j<len-i-1; ++j){
-                if(arrayToSort[j+1]<arrayToSort[j]){
+    public int[] bubbleSort(int[] arrayToSort) {
+        int len = arrayToSort.length;
+        for (int i = 0; i < len - 1; ++i) {
+            for (int j = 0; j < len - i - 1; ++j) {
+                if (arrayToSort[j + 1] < arrayToSort[j]) {
                     int swap = arrayToSort[j];
-                    arrayToSort[j] = arrayToSort[j+1];
-                    arrayToSort[j+1] = swap;
+                    arrayToSort[j] = arrayToSort[j + 1];
+                    arrayToSort[j + 1] = swap;
                 }
             }
         }
         return arrayToSort;
+    }
+
+    @Override
+    public int[] mergeSort(int[] arrayToSort) {
+        int len = arrayToSort.length;
+        // very if len >=1
+        if (len <= 1) {
+            return arrayToSort;
+        }
+        //need to add some left and right functions
+        //
+       // int[] left  = new int[];
+        return new int[0];
+
     }
 }
